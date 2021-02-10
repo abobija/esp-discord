0ae651580502f89c27a35a1c7455d8d23ca62b81
abobija <abobijax@gmail.com>
2021-02-10 18:31:46 +0100

    Intents as defines

--- discord.h
+++ discord.h
@@ -6,6 +6,22 @@
 extern "C" {
 #endif
 
+#define DISCORD_INTENT_GUILDS                    (1 << 0)
+#define DISCORD_INTENT_GUILD_MEMBERS             (1 << 1)
+#define DISCORD_INTENT_GUILD_BANS                (1 << 2)
+#define DISCORD_INTENT_GUILD_EMOJIS              (1 << 3)
+#define DISCORD_INTENT_GUILD_INTEGRATIONS        (1 << 4)
+#define DISCORD_INTENT_GUILD_WEBHOOKS            (1 << 5)
+#define DISCORD_INTENT_GUILD_INVITES             (1 << 6)
+#define DISCORD_INTENT_GUILD_VOICE_STATES        (1 << 7)
+#define DISCORD_INTENT_GUILD_PRESENCES           (1 << 8)
+#define DISCORD_INTENT_GUILD_MESSAGES            (1 << 9)
+#define DISCORD_INTENT_GUILD_MESSAGE_REACTIONS   (1 << 10)
+#define DISCORD_INTENT_GUILD_MESSAGE_TYPING      (1 << 11)
+#define DISCORD_INTENT_DIRECT_MESSAGES           (1 << 12)
+#define DISCORD_INTENT_DIRECT_MESSAGE_REACTIONS  (1 << 13)
+#define DISCORD_INTENT_DIRECT_MESSAGE_TYPING     (1 << 14)
+
 typedef struct {
     char* token;
     int intents;
