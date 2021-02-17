5165ce99d15aa34f9a549f49e7d6a367ca67d1d4
abobija <abobijax@gmail.com>
2021-02-17 15:21:20 +0100

    discord_message_send args checks

--- src/discord/messages.c
+++ src/discord/messages.c
@@ -5,7 +5,20 @@
 DISCORD_LOG_DEFINE_BASE();
 
 esp_err_t discord_message_send(discord_client_handle_t client, discord_message_t* message) {
+    if(message == NULL)
+        return ESP_ERR_INVALID_ARG;
+
     DISCORD_LOG_FOO();
+
+    if(message->content == NULL) {
+        DISCORD_LOGE("Missing content");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if(message->channel_id == NULL) {
+        DISCORD_LOGE("Missing channel id");
+        return ESP_ERR_INVALID_ARG;
+    }
 
     DCAPI_POST(
         STRCAT("/channels/", message->channel_id, "/messages"),
