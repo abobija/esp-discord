ded0196846c3b8208f3a8cb6d1a685f8a20d4adf
abobija <abobijax@gmail.com>
2021-02-28 12:27:19 +0100

    Modify certgen script to work on windows Git-Bash

--- certgen.sh
+++ certgen.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 discord_cert_gen() {
-    echo "" | openssl s_client -showcerts -connect $1:443 2> /dev/null | sed -n "1,/Root/d; /BEGIN/,/END/p" | openssl x509 -outform PEM > $2
+    echo "" | openssl s_client -showcerts -connect $1:443 2> /dev/null | sed -n "1,/Root/d; /BEGIN/,/END/p" | openssl x509 -outform PEM > "$2"
 }
 
 discord_certs() {
-    local PWD=$(pwd)/$(dirname $BASH_SOURCE)
-    local CRT_DIR="${PWD}/cert"
-
-    mkdir ${CRT_DIR} 2> /dev/null
+	local DIRNAME=$(dirname "${BASH_SOURCE}")
+    local CRT_DIR="${DIRNAME}/cert"
+	
+    mkdir "${CRT_DIR}" 2> /dev/null
     
     echo "Generating gateway certificate..."
-    discord_cert_gen gateway.discord.gg ${CRT_DIR}/gateway.pem
+    discord_cert_gen gateway.discord.gg "${CRT_DIR}/gateway.pem"
 
     if [[ $? == 0 ]]; then
         echo "Generated."
         echo "Generating api certificate..."
-        discord_cert_gen discord.com ${CRT_DIR}/api.pem
+        discord_cert_gen discord.com "${CRT_DIR}/api.pem"
     fi
 
     if [[ $? == 0 ]]; then
