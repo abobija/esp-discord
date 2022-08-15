0abfa69a625164575456a03a1dfc267bf444fc89
abobija <abobijax@gmail.com>
2022-08-15 14:36:21 +0200

    Comments

--- include/discord/embed.h
+++ include/discord/embed.h
@@ -8,16 +8,16 @@ extern "C" {
 #include "discord.h"
 
 typedef struct {
-    char* text;      /*!< Footer text */
-    char* icon_url;  /*!< Url of footer icon (only supports http(s) and attachments) */
+    char* text;                     /*!< Footer text */
+    char* icon_url;                 /*!< Url of footer icon (only supports http(s) and attachments) */
 } discord_embed_footer_t;
 
 typedef struct {
-    char* title;
-    char* description;
-    char* url;
-    int color;
-    discord_embed_footer_t* footer;
+    char* title;                    /*!< Title of embed. Limit: 256 */
+    char* description;              /*!< Description of embed. Limit: 4096 */
+    char* url;                      /*!< Url of embed */
+    int color;                      /*!< Color code of the embed. DISCORD_COLOR_* predefines can be used */
+    discord_embed_footer_t* footer; /*!< Footer information */
 } discord_embed_t;
 
 void discord_embed_free(discord_embed_t* embed);
